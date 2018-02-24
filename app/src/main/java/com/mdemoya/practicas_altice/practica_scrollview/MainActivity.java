cbc54402f16fceff35eac0778191ca1642dedd88
Marcos De Moya <marcosdemoyalpz@gmail.com>
2018-02-24 12:50:52 -0400

    Initial commit

---
+++ app/src/main/java/com/mdemoya/practicas_altice/practica_scrollview/MainActivity.java
@@ -0,0 +1,13 @@
+package com.mdemoya.practicas_altice.practica_scrollview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
